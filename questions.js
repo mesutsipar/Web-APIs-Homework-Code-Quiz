b8a72ffd124b8732ff110891163413fe0614e662
Mesut Sipar <mesutsipar@gmail.com>
2020-01-22 01:56:43 -0600

    adding homework

---
+++ questions.js
@@ -0,0 +1,31 @@
+var questions = [
+    {
+        title: "What is the name of Frodo and the other hobbit's home?",
+        choices: ["The Hole", "The Hideout", "The Hollow", "The Shire"],
+        answer: "The Shire"
+    },
+    {
+        title: "How many members are in the Fellowship?",
+        choices: ["Nine", "Eight", "Four", "Twelve"],
+        answer: "Nine"
+    },
+    {
+        title: "How does Frodo get the ring?",
+        choices: ["Finds it in the bushes", "Steals it", "Given to him by Bilbo", "Sent to him in the mail"],
+        answer: "Given to him by Bilbo"
+    },
+    {
+        title: "How does Gandalf escape Saruman's prison?",
+        choices: ["Frodo sneaks him out", "Kills him", "An eagle", "Magic"],
+        answer: "An eagle"
+    },
+    {
+        title: "Where is the only place that the One Ring can be properly destroyed?",
+        choices: ["Fires of mount doom", "In the water of the shire", "Rivendell", "Caves of misty mountain"],
+        answer: "Fires of mount doom"
+    },
+
+
+];
+
+   
